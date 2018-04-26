a5a89f860fbc7c680e83d5886c83907b02fbd86b
Manu Seth <mseth@eng.ucsd.edu>
2018-04-26 15:00:48 -0700

    Update OFDM_RX.m
    Some more hints on CFO and SFO, and slight change of structure

--- OFDM_RX.m
+++ OFDM_RX.m
@@ -1,31 +1,38 @@
 
 %% Rx processing params
 
-rx_data= raw_rx_dec; % run OFDM tx code to get raw_rx_dec
-LTS_CORR_THRESH               = 0.8;         % Normalized threshold for LTS correlation
+rx_data = raw_rx_dec;          % run OFDM tx code to get raw_rx_dec
+LTS_CORR_THRESH = 0.8;         % Normalized threshold for LTS correlation
 
 % Repeat the following code for each packet
 
 %% Packet Detection
+% Cross correlation of received signal with LTS
 
-% Cross correlation of received signal with LTS or STS
-% With LTS: Can use the thesis for reference
-% With STS: You can read the paper 'Robust Frequency and Timing
-% Synchronization for OFDM' by Timothy M. Schmidl and Donald C. Cox
 
 %% CFO estimation and correction
+% Use two copies of LTS for cross-correlation
 
 
-%% Channel estimation
+%% CP Removal
 
 
-%% Rx payload processing
+%% FFT
+
+
+%% Channel estimation and correction
 
 
 %% SFO estimation and correction using pilots
-
+% SFO manifests as a frequency-dependent phase whose slope increases
+% over time as the Tx and Rx sample streams drift apart from one
+% another. To correct for this effect, we calculate this phase slope at
+% each OFDM symbol using the pilot tones and use this slope to
+% interpolate a phase correction for each data-bearing subcarrier.
+% Extract the pilot tones and "equalize" them by their nominal Tx values
 
 %% Phase Error Correction
+% Extract the pilots and calculate per-symbol phase error
 
 
 %% Demodulation
