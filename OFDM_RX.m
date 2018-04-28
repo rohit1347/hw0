d67793014d3d6aa89255af941579611d0dbb220f
Manu Seth <manuseth1010@gmail.com>
2018-04-28 04:47:03 -0700

    comments and outputs of each block

--- OFDM_RX.m
+++ OFDM_RX.m
@@ -3,24 +3,42 @@
 
 rx_data = raw_rx_dec;          % run OFDM tx code to get raw_rx_dec
 LTS_CORR_THRESH = 0.8;         % Normalized threshold for LTS correlation
+% Usage: Find all peaks whose magnitude is greater than 0.8 times
+% the maximum magnitude after cross correlation (Packet Detection)
 
 % Repeat the following code for each packet
 
 %% Packet Detection
 % Cross correlation of received signal with LTS
 
+% Output: Single packet extracted from rx_data
+% with knowledge of preamble (LTS) indices and payload vector indices
+
 
 %% CFO estimation and correction
-% Use two copies of LTS for cross-correlation
+% Use two copies of LTS for cross-correlation (Reference: Thesis)
+
+% Output: Packet with each value multiplied by CFO correction factor
 
 
 %% CP Removal
+% Refer to the process used to add CP at TX
+% Converting vector back to matrix form will help
+
+% Output: CP free payload matrix of size (N_SC * N_OFDM_SYMS)
 
 
 %% FFT
+% Refer to IFFT perfomed at TX
+
+% Output: Symbol matrix in frequency domain of same size
 
 
 %% Channel estimation and correction
+% Use the two copies of LTS and find channel estimate (Reference: Thesis)
+% Convert channel estimate to matrix form and equlaize the above matrix
+
+% Output : Symbol equalized matrix in frequency domain of same size
 
 
 %% SFO estimation and correction using pilots
@@ -29,10 +47,15 @@ LTS_CORR_THRESH = 0.8;         % Normalized threshold for LTS correlation
 % another. To correct for this effect, we calculate this phase slope at
 % each OFDM symbol using the pilot tones and use this slope to
 % interpolate a phase correction for each data-bearing subcarrier.
-% Extract the pilot tones and "equalize" them by their nominal Tx values
 
-%% Phase Error Correction
+% Output: Symbol equalized matrix with pilot phase correction applied
+
+
+%% Phase Error Correction using pilots
 % Extract the pilots and calculate per-symbol phase error
+
+% Output: Symbol equalized matrix with pilot phase correction applied
+% Remove pilots and flatten the matrix to a vector rx_syms
 
 
 %% Demodulation
@@ -41,9 +64,9 @@ LTS_CORR_THRESH = 0.8;         % Normalized threshold for LTS correlation
 Demap_out = demapper(rx_syms,mod_type,1);
 
 % viterbi decoder
-rx_data=vitdec(Demap_out,trel,7,'trunc','hard');
+rx_data_final = vitdec(Demap_out,trel,7,'trunc','hard');
 
 % rx_data is the final output corresponding to tx_data, which can be used
 % to calculate BER
 
-[number,ber] = biterr(tx_data,rx_data);
+[number,ber] = biterr(tx_data,rx_data_final);
